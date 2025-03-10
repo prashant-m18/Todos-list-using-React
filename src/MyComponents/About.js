bb27b5f3fb7144d12b1b926428582ff439dd355e
Prashant Mishra <prashantmishravg@gmail.com>
2025-03-10 23:27:18 +0530

    feature update

---
+++ src/MyComponents/About.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const About = () => {
+  return (
+    <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi odio amet nesciunt rem dolor tempora cumque, eveniet numquam dignissimos nam eos quas magni est temporibus architecto dolorum error maxime non cum autem. Dolor, quae?</div>
+  )
+}
